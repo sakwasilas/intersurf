f62dff1f1c5cecc14412c6fd2b62b663acb20a06
sakwasilas <sakwahsilas@gmail.com>
2025-10-22 08:47:37 +0300

    updated web

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    // Toggle mobile menu
+    const toggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    toggle.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    });
