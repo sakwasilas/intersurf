96a161f3dd0cac5519731290cdb6e81205ca0efc
sakwasilas <sakwahsilas@gmail.com>
2025-10-23 00:06:35 +0300

    done with it

--- script.js
+++
@@ -1,8 +0,0 @@
-
-    // Toggle mobile menu
-    const toggle = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
-
-    toggle.addEventListener('click', () => {
-      menu.classList.toggle('show');
-    });
